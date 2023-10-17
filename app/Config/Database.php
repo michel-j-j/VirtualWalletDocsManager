d1392ab2c5a30d8b5549111ce2a96a84f5762bea
michel-j-j <asesinevil@gmail.com>
2023-10-17 16:57:35 -0300

    Cambios

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'proyecto_software_final',
+        'database'     => 'proyecto_software',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
