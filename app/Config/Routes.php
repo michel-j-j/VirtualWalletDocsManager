ce5112e4b2ea41f11559f78f44cf03f12c8db6c9
Sektormk3u <sektormk3u@gmail.com>
2023-11-01 16:52:38 -0300

    commit leo

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/pantallaTesting', 'AdminController::index');
+// $routes->get('/pantallaTesting', 'AdminController::index');
 
 $routes->get('/login', 'LoginController::login');
 $routes->post('/login/logear', 'LoginController::logear');
